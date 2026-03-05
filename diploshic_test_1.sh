738b1bb3c8554fa28c325fda6de9f0708aa4e3b3
Luke Scott <lukescottarthur@gmail.com>
2026-03-05 09:42:46 -0500

    diploshic tested

--- diploshic_test_1.sh
+++ diploshic_test_1.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks=1                                  # 
 #SBATCH --cpus-per-task=8                           # CPU core count per task
 #SBATCH --mem=16G                                    # Memory per node
-#SBATCH --time=06:30:00                              # Time limit hrs:mins:secs
-#SBATCH --output=/home/las80898/diploSHIC/%x_%j.out  
-#SBATCH --error=/home/las80898/diploSHIC/%x_%j.error 
+#SBATCH --time=03:30:00                              # Time limit hrs:mins:secs
+#SBATCH --output=/home/las80898/diploSHIC/%x_%j.out  ####change to your path
+#SBATCH --error=/home/las80898/diploSHIC/%x_%j.error ####change to your path
 #SBATCH --mail-user=las80898@uga.edu                # Where to send mail
 #SBATCH --mail-type=BEGIN,END,FAIL                        # Mail events 
 
@@ -15,8 +15,26 @@ CONDA_BASE=$(conda info --base)
 source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate diplo_env
 
-# Change directory 
+# Navigate to diploSHIC directory (change to your directory)
 cd /home/las80898/diploSHIC
 
 for f in exampleApplication/*.msOut.gz; do diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz --chrArmsForMasking 3R 
 done
+
+# make balanced training set
+mkdir rawFVFiles
+mv exampleApplication/*.fvec rawFVFiles/
+mkdir trainingSets
+diploSHIC makeTrainingSets rawFVFiles/neut.msOut.gz.diploid.fvec rawFVFiles/soft rawFVFiles/hard 5 0,1,2,3,4,6,7,8,9,10 trainingSets/
+
+# train! (kind of long)
+diploSHIC train trainingSets/ trainingSets/ bfsModel
+
+# feature vectors for real data
+diploSHIC fvecVcf diploid exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz 3R 53200684 exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec --targetPop BFS --sampleToPopFileName exampleApplication/samples_pops.txt --winSize 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz
+
+# move output
+mv exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec
+
+# prediction on empirical feature vectors
+diploSHIC predict bfsModel.json bfsModel.weights.h5 rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec mossie.preds
