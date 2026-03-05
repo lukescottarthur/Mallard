8fd6b919af1cba6f843122f129b3db13b7f8eca0
Luke Scott <lukescottarthur@gmail.com>
2026-03-04 21:08:09 -0500

    more sh

--- diploshic_test_1.sh
+++ diploshic_test_1.sh
@@ -18,8 +18,25 @@ conda activate diplo_env
 # Change directory 
 cd /home/las80898/diploSHIC
 
-for f in exampleApplication/*.msOut.gz 
-do 
-diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz --chrArmsForMasking 3R 
-& 
-done
+for f in exampleApplication/*.msOut.gz; do diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz --chrArmsForMasking 3R & done
+
+mkdir rawFVFiles && mv exampleApplication/*.fvec rawFVFiles/
+mkdir trainingSets
+diploSHIC makeTrainingSets rawFVFiles/neut.msOut.gz.diploid.fvec rawFVFiles/soft \
+rawFVFiles/hard 5 0,1,2,3,4,6,7,8,9,10 trainingSets/
+
+
+diploSHIC train trainingSets/ trainingSets/ bfsModel
+
+diploSHIC fvecVcf diploid \
+    exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz \
+    3R 53200684 \
+    exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec \
+    --targetPop BFS \
+    --sampleToPopFileName exampleApplication/samples_pops.txt \
+    --winSize 55000 \
+    --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz
+
+diploSHIC predict bfsModel.json bfsModel.weights.hdf5 rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec mossie.preds
+
+
