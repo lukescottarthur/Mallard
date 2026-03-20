4a74fe775e23e71d12e22f84128fffca01d6b78a
Luke Scott <lukescottarthur@gmail.com>
2026-03-20 13:10:35 -0400

    diplo scripting

---
+++ diploshic_mallard1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=diploshic_mallard1                      # Job name 
+#SBATCH --partition=batch                           # Partition name 
+#SBATCH --ntasks=1                                  # 
+#SBATCH --cpus-per-task=16                           # CPU core count per task
+#SBATCH --mem=32G                                    # Memory per node
+#SBATCH --time=24:00:00                              # Time limit hrs:mins:secs
+#SBATCH --output=/home/las80898/Mallard/%x_%j.out  ####change to your path
+#SBATCH --error=/home/las80898/Mallard/%x_%j.error ####change to your path
+#SBATCH --mail-user=las80898@uga.edu                #####change to your path
+#SBATCH --mail-type=END,FAIL                        # Mail events 
+
+# Load conda environment 
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate diplo_env
+
+# Navigate to diploSHIC directory (change to your directory)
+cd /home/las80898/diploSHIC
+
+# chr17 name
+# chr17 length 215745
+
+#calculate feature vectors from simulations
+for f in mallardtest1/*.msOut.gz; do diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000
+done
+
+# make balanced training set
+mkdir rawFVFiles
+mv mallardtest1/*.fvec rawFVFiles/
+mkdir mallardTrainingSets
+diploSHIC makeTrainingSets rawFVFiles/neut.msOut.gz.diploid.fvec rawFVFiles/soft rawFVFiles/hard 5 0,1,2,3,4,6,7,8,9,10 mallardTrainingSets/
+
+# train!
+diploSHIC train mallardTrainingSets/ mallardTrainingSets/ bfsModel
+
+# feature vectors for real data
+diploSHIC fvecVcf diploid mallardtest1/chr17merge.vcf.gz chr17 215745 mallardtest1/chr17merge.fvec --winSize 55000
+
+# move output
+mv mallardtest1/chr17merge.fvec rawFVFiles/chr17merge.fvec
+
+# prediction on empirical feature vectors
+diploSHIC predict bfsModel.json bfsModel.weights.h5 rawFVFiles/chr17merge.fvec mallardtest1_results
--- diploshic_test_1.sh
+++ diploshic_test_1.sh
@@ -18,8 +18,9 @@ conda activate diplo_env
 # Navigate to diploSHIC directory (change to your directory)
 cd /home/las80898/diploSHIC
 
+
 #calculate feature vectors from simulations
-for f in exampleApplication/*.msOut.gz; do diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz --chrArmsForMasking 3R 
+for f in exampleApplication/*_mallard1.gz; do diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz --chrArmsForMasking 3R 
 done
 
 # make balanced training set
